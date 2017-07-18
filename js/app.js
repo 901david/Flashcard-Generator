196bb1de1bf3bbbdf008f9ec903a27b2b78ec15b
DHLAP\h <vdavidhammond@gmail.com>
2017-07-18 16:39:08 -0600

    Introduced background and began on logic created index page that will eventually be a login

--- js/app.js
+++ js/app.js
@@ -8,8 +8,22 @@
  // var questionFive = basicData.basicCard("What's the opposite of hot?", "Cold.");
  // var cozeQOne = clozeData.clozeCard("George Washington was the first president of the United States", "George Washington");
 
- // Handles on click of sign in button pops up modal for sign in.
- $("#signInSecondary").on("click", function () {
-   alert("You clicked the sign in button");
- });
- $("#navBar").html("<h1> THis WOrked</h1>");
+
+// This will display the choices and determine which type of card to create
+function whichTypeOfCard () {
+  let formGeneration = '<div id="basicInput" class="hide"><label for="example-text-input" class="col-2 col-form-label">Enter Data for Front of Card (Question)</label><div class="col-4"><input class="form-control" type="text" value="Enter Front of Card Data" id="frontCardData"></div><label for="example-text-input" class="col-2 col-form-label">Enter Data for Back of Card (Answer)</label><div class="col-4"><input class="form-control" type="text" value="Enter Back of Card data" id="backCardData"></div></div>';
+  let whatDoYouChoose = "<h3>What type of flash card would you like to create?</h3><br/>" + formGeneration + "<div data-Choice='basic' class='btn btn-success' id='basicCardSelector'>Basic Card</div><div class='btn btn-warning' id='clozeCardSelector'>Cloze Card</div>";
+    $("#displayCards").append(whatDoYouChoose);
+    console.log(whatDoYouChoose);
+    $("#basicCardSelector").on("click", function () {
+      $("#displayCards").empty();
+      
+    });
+    $("#clozeCardSelector").on("click", function () {
+
+    });
+    function whatWasYourChoice (dataIdArg) {
+
+    };
+};
+whichTypeOfCard();
