34009343f56c1e0430a59c554e6c1d3f25471969
DHLAP\h <vdavidhammond@gmail.com>
2017-07-18 16:58:37 -0600

    Fixed the Cloze Card logic - need to address the $ not working

--- js/ClozeCard.js
+++ js/ClozeCard.js
@@ -3,9 +3,10 @@ exports.clozeCard = function(text, cloze) {
   this.text = text;
   this.answer = cloze;
   if (this.text.includes(this.answer)) {
-    var cardFront = this.text.replace(cloze, "...");
+    var cardFront = this.text.replace(this.answer, "...");
     console.log("Question is: " + cardFront);
-    console.log("THe answer is: " + this.answer);
+    console.log("THe answer is: " + cardFront.replace("...", this.answer));
+
   }
   else {
     console.log("Question not formatted properly.");
--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-// var clozeData = require("./ClozeCard.js");
-// var basicData = require("./BasicCard.js");
+var clozeData = require("./ClozeCard.js");
+var basicData = require("./BasicCard.js");
 // Basic Questions
  // var questionOne = basicData.basicCard("What is the square root of 16?", "4.");
  // var questionTwo = basicData.basicCard("What is 4 x 5?", "20.");
  // var questionThree = basicData.basicCard("What is 20 / 2?", "10.");
  // var questionFour = basicData.basicCard("Who was the first President?", "George Washington.");
  // var questionFive = basicData.basicCard("What's the opposite of hot?", "Cold.");
- // var cozeQOne = clozeData.clozeCard("George Washington was the first president of the United States", "George Washington");
+ var cozeQOne = clozeData.clozeCard("George Washington was the first president of the United States", "George Washington");
 
 
 // This will display the choices and determine which type of card to create
@@ -17,7 +17,7 @@ function whichTypeOfCard () {
     console.log(whatDoYouChoose);
     $("#basicCardSelector").on("click", function () {
       $("#displayCards").empty();
-      
+
     });
     $("#clozeCardSelector").on("click", function () {
 
