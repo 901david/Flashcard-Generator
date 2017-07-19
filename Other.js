f95d3ec6ffd34dc149fe5aa9003c4a136902f1b7
DHLAP\h <vdavidhammond@gmail.com>
2017-07-19 16:35:17 -0600

    Moved all html Js into new file to parse through - got app more functional in node

---
+++ Other.js
@@ -0,0 +1,72 @@
+const http = require('http')
+const port = 8000
+
+const requestHandler = (request, response) => {
+  console.log(request)
+  response.end('This is a single response server.  Above, you can write, response.write(\'write something here...\');')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+});
+
+var variableNumber = 0;
+var cardCreatedArray = [];
+var currentVariableName;
+var currentIndex = variableNumber;
+
+// Gives us a continuing variable scheme
+function whatVariableToUse () {
+  variableNumber++;
+  currentVariableName = "card" + variableNumber;
+  cardCreatedArray.push(currentVariableNa);
+  console.log(cardCreatedArray);
+
+
+};
+
+// Determines what type of card to create based on which button is clicked.
+function grabDataAndRun (){
+  variableName = currentVariableName;
+  $(".clickHerePlease").click(function() {
+    console.log("You clicked me");
+    let questionArg = $("#frontCardData").val();
+    let answerArg = $("#backCardData").val();
+    let dataArg = $(this).attr("data-Choice");
+    console.log(dataArg);
+    if(dataArg === "basic") {
+      console.log("You clicked me");
+      whatVariableToUse();
+
+      // HOw do I actually make this the value of currentVariableName
+      // cardCreatedArray[currentIndex] = basicData.basicCard("'" + questionArg + "'", "'" + answerArg + "'");
+        console.log(cardCreatedArray);
+    }
+    else if (dataArg === "cloze") {
+      console.log("You clicked me");
+      whatVariableToUse();
+    }
+    else {
+      console.log("This means something went fatally wrong.  Error.");
+    }
+
+});
+};
+
+
+
+// <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="displayCards">
+//
+// <img src="images/indexfront.jpg" alt"Front of Index Card" class="img-responsive pull-left imgBorder">
+// <img src="images/indexback.jpg" alt"Back of Index Card" class="img-responsive pull-right imgBorder">
+// </div>
+$(document).ready(function() {
+grabDataAndRun(currentVariableName);
+
+});
--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-// var clozeData = require("./ClozeCard.js");
-// var basicData = require("./BasicCard.js");
+
+
+var clozeData = require("./ClozeCard.js");
+var basicData = require("./BasicCard.js");
+var argOne = process.argv[2];
+var argTwo = process.argv[3];
+
+// console.log("argOne: " + argOne + " " + "argTwo: " + argTwo);
 
 // Basic Questions
  // var questionOne = basicData.basicCard("What is the square root of 16?", "4.");
@@ -10,57 +16,23 @@
  // var clozeQOne = clozeData.clozeCard("George Washington was the first president of the United States", "George Washington");
  // var clozeQTwo = clozeData.clozeCard("Four is the square root of 16.", "Four");
  // var basicQ = basicData.basicCard("Why is having children so diffcult?", "Because it drains your energy, steals your soul, and depletes your piggy bank");
-var variableNumber = 0;
-var cardCreatedArray = [];
-var currentVariableName;
-var currentIndex = variableNumber;
+function createBasicQ (basicFront, basicBack) {
+    if ((basicFront === undefined) || (basicBack === undefined)) {
+      console.log("You did not enter the correct specifications.  Try again.");
 
-// Gives us a continuing variable scheme
-function whatVariableToUse () {
-  variableNumber++;
-  currentVariableName = "card" + variableNumber;
-  cardCreatedArray.push(currentVariableNa);
-  console.log(cardCreatedArray);
-
-
-};
-
-// Determines what type of card to create based on which button is clicked.
-function grabDataAndRun (){
-  variableName = currentVariableName;
-  $(".clickHerePlease").click(function() {
-    console.log("You clicked me");
-    let questionArg = $("#frontCardData").val();
-    let answerArg = $("#backCardData").val();
-    let dataArg = $(this).attr("data-Choice");
-    console.log(dataArg);
-    if(dataArg === "basic") {
-      console.log("You clicked me");
-      whatVariableToUse();
-
-      // HOw do I actually make this the value of currentVariableName
-      // cardCreatedArray[currentIndex] = basicData.basicCard("'" + questionArg + "'", "'" + answerArg + "'");
-        console.log(cardCreatedArray);
-    }
-    else if (dataArg === "cloze") {
-      console.log("You clicked me");
-      whatVariableToUse();
     }
     else {
-      console.log("This means something went fatally wrong.  Error.");
+      var randomQ = basicData.basicCard(basicFront, basicBack);
     }
-
-});
 };
+function createCloze (clozeFront, clozeBack) {
+  if ((clozeFront === undefined) || (clozeBack === undefined)) {
+    console.log("You did not enter the correct specifications.  Try again.");
 
-
-
-// <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="displayCards">
-//
-// <img src="images/indexfront.jpg" alt"Front of Index Card" class="img-responsive pull-left imgBorder">
-// <img src="images/indexback.jpg" alt"Back of Index Card" class="img-responsive pull-right imgBorder">
-// </div>
-$(document).ready(function() {
-grabDataAndRun(currentVariableName);
-
-});
+  }
+  else {
+    var randomQ2 = clozeData.clozeCard(clozeFront, clozeBack);
+  }
+};
+// createBasicQ(argOne, argTwo);
+createCloze(argOne, argTwo);
