c9434976543e05e8947b5f58613a25c83e64549a
DHLAP\h <vdavidhammond@gmail.com>
2017-07-20 07:31:14 -0600

    Transforming this into a node app only with inquirer remving all web related files

--- js/app.js
+++ js/app.js
@@ -5,17 +5,6 @@ var basicData = require("./BasicCard.js");
 var argOne = process.argv[2];
 var argTwo = process.argv[3];
 
-// console.log("argOne: " + argOne + " " + "argTwo: " + argTwo);
-
-// Basic Questions
- // var questionOne = basicData.basicCard("What is the square root of 16?", "4.");
- // var questionTwo = basicData.basicCard("What is 4 x 5?", "20.");
- // var questionThree = basicData.basicCard("What is 20 / 2?", "10.");
- // var questionFour = basicData.basicCard("Who was the first President?", "George Washington.");
- // var questionFive = basicData.basicCard("What's the opposite of hot?", "Cold.");
- // var clozeQOne = clozeData.clozeCard("George Washington was the first president of the United States", "George Washington");
- // var clozeQTwo = clozeData.clozeCard("Four is the square root of 16.", "Four");
- // var basicQ = basicData.basicCard("Why is having children so diffcult?", "Because it drains your energy, steals your soul, and depletes your piggy bank");
 function createBasicQ (basicFront, basicBack) {
     if ((basicFront === undefined) || (basicBack === undefined)) {
       console.log("You did not enter the correct specifications.  Try again.");
