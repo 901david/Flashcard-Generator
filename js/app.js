b70c914944b46764b1f97c0d9bea2f92cc384a57
DHLAP\h <vdavidhammond@gmail.com>
2017-07-20 20:32:01 -0600

    Added in Inquirer interface. You can decide how many cards to create and which type

--- js/app.js
+++ js/app.js
@@ -2,17 +2,20 @@
 
 var clozeData = require("./ClozeCard.js");
 var basicData = require("./BasicCard.js");
+var inquirer = require("inquirer");
 var argOne = process.argv[2];
 var argTwo = process.argv[3];
+var recNum;
+var count = 0;
 
 function createBasicQ (basicFront, basicBack) {
-    if ((basicFront === undefined) || (basicBack === undefined)) {
-      console.log("You did not enter the correct specifications.  Try again.");
+  if ((basicFront === "") || (basicBack === "")) {
+    console.log("You did not enter the correct specifications.  Try again.");
 
-    }
-    else {
-      var randomQ =new basicData.BasicCard(basicFront, basicBack);
-    }
+  }
+  else {
+    var randomQ =new basicData.BasicCard(basicFront, basicBack);
+  }
 };
 function createCloze (clozeFront, clozeBack) {
   if ((clozeFront === undefined) || (clozeBack === undefined)) {
@@ -23,5 +26,63 @@ function createCloze (clozeFront, clozeBack) {
     var randomQ2 =new clozeData.ClozeCard(clozeFront, clozeBack);
   }
 };
-// createBasicQ(argOne, argTwo);
-createBasicQ(argOne, argTwo);
+inquirer.prompt([
+  {
+    name: "cardNumber",
+    message: "How many cards total do you want to make?"
+  }]).then(function(carNumObj) {
+    recNum = carNumObj.cardNumber;
+    console.log(carNumObj);
+    function askQuestion () {
+
+      if (count < recNum) {
+        console.log("Count: " + count + " " + "RecNUm: " + recNum);
+      inquirer.prompt([
+        {
+          type: "list",
+          name: "type",
+          message: "What type of Flashcard do you want to create?",
+          choices: ["Basic Card", "Cloze Card"]
+        }]).then(function(carTypeObj) {
+          cardType = carTypeObj.type;
+          console.log(carTypeObj);
+          if (cardType === "Cloze Card"){
+            inquirer.prompt([
+              {
+                name: "frontCardCloze",
+                message: "What should the Question be?"
+              }, {
+                name: "backCardCloze",
+                message: "What part do you want to remove? (Must match exactly)"
+              }]).then(function(cardDataClozeObj) {
+                createCloze(cardDataClozeObj.frontCardCloze, cardDataClozeObj.backCardCloze);
+                count++;
+                askQuestion();
+
+              });
+          }
+          else {
+            inquirer.prompt([
+              {
+                name: "frontCardBasic",
+                message: "What should the Question be?"
+              }, {
+                name: "backCardBasic",
+                message: "What should the answer be?"
+              }]).then(function(cardDataBasicObj) {
+                  createBasicQ(cardDataBasicObj.frontCardBasic, cardDataBasicObj.backCardBasic);
+                  count++;
+                  askQuestion();
+              });
+          }
+
+
+        });
+      }
+      else {
+        console.log("All cards created");
+      }
+      };
+
+        askQuestion();
+    });
